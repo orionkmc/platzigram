9ce59dc017519666fe16bbb6ac4e00589ec2a234
Kuai-mare <orionkmc@gmail.com>
2017-05-24 19:20:16 -0400

    haciendo request tipo ajax con superagent

--- server.js
+++ server.js
@@ -24,6 +24,34 @@ app.get('/signin', function(req, res){
   res.render('index', { title: 'Platzigram - Signup' });
 });
 
+app.get('/api/pictures', function(req, res, next){
+  var pictures = [
+      {
+        user: {
+          username: 'Kuai-mare',
+          avatar: 'https://pbs.twimg.com/profile_images/541801670397550592/yEdF9WuK_400x400.jpeg'
+        },
+        url: 'office.jpg',
+        likes: 15,
+        liked: false,
+        createdAt: new Date().getTime()
+      },
+      {
+        user: {
+          username: 'Arturo',
+          avatar: 'https://pbs.twimg.com/profile_images/500645664867778560/MNCJkvbD_400x400.jpeg'
+        },
+        url: 'office.jpg',
+        likes: 5,
+        liked: true,
+        createdAt: new Date().setDate(new Date().getDate() - 10)
+      }
+    ];
+
+    setTimeout(function(){
+      res.send(pictures);
+    }, 2000);
+  })
 // Se lanza el servidor en el puerto especificado
 app.listen(3000, function (err){
   if (err) return console.log('Hubo un error'), process.exit(1);
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,31 +2,22 @@ var page      = require('page');
 var empty     = require('empty-element');
 var title     = require('title');
 var template  = require('./template');
+var request   = require('superagent');
 
-page('/', function(ctx, next){
+page('/', loadPictures, function(ctx, next){
   title('Platzigram - Home');
   var main  = document.getElementById('main-container');
-  var pictures = [
-    {
-      user: {
-        username: 'Kuai-mare',
-        avatar: 'https://pbs.twimg.com/profile_images/541801670397550592/yEdF9WuK_400x400.jpeg'
-      },
-      url: 'office.jpg',
-      likes: 15,
-      liked: false,
-      createdAt: new Date().getTime()
-    },
-    {
-      user: {
-        username: 'Arturo',
-        avatar: 'https://pbs.twimg.com/profile_images/500645664867778560/MNCJkvbD_400x400.jpeg'
-      },
-      url: 'office.jpg',
-      likes: 5,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate() - 10)
-    }
-  ];
-  empty(main).appendChild(template(pictures));
+
+  empty(main).appendChild(template(ctx.pictures));
 });
+
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function (err, res) {
+      if (err) return console.log(err);
+
+      ctx.pictures = res.body;
+      next();
+    })
+}
