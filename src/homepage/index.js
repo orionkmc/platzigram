3f323a7621f43925b180fac288f35b371f45d5b4
Kuai-mare <orionkmc@gmail.com>
2017-05-24 13:29:32 -0400

    creando el timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,9 +1,11 @@
-var page  = require('page');
-var empty = require('empty-element');
-var title = require('title');
+var page      = require('page');
+var empty     = require('empty-element');
+var title     = require('title');
+var template  = require('./template');
 
 page('/', function(ctx, next){
   title('Platzigram - Home');
   var main  = document.getElementById('main-container');
   main.innerHTML = 'Home <a href="/signup">Signup</a> <a href="/signin">Signin</a>';
+  empty(main).appendChild(template);
 });
