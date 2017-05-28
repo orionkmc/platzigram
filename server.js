65123119267238490ff5cd10488e6be85aa3d8d0
Kuai-mare <orionkmc@gmail.com>
2017-05-28 18:56:49 -0400

    agregando funcionalidad para almacenar imagenes en el servidor

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var multer = require('multer');
 var ext = require('file-extension');
+//var aws = require('aws-sdk');
+//var multerS3 = require('multer-s3');
+//var config = require('./config');
 
+/*var s3 = new aws.S3({
+  accessKeyId: config.aws.accessKey,
+  secretAccessKey: config.aws.secretKey
+})
+
+var storage = multerS3({
+  s3: s3,
+  bucket: 'orion-platzigram',
+  acl: 'public-read',
+  metadata: function(req, file, cb){
+    cb(null, { fielName: file.fieldname })
+  },
+  key: function(req, file, cb){
+    cb(null, +Date.now() +'.'+ ext(file.originalname))
+  }
+});*/
 var storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, './uploads')
@@ -10,7 +29,6 @@ var storage = multer.diskStorage({
         cb(null, +Date.now() +'.'+ ext(file.originalname))
     }
 })
-
 var upload = multer({ storage: storage }).single('picture');
 
 var app   = express();
@@ -69,7 +87,7 @@ app.get('/api/pictures', function(req, res, next){
 app.post('/api/pictures', function(req, res){
     upload(req, res, function(err){
         if (err) {
-            return res.send(500, "Error uploading file");
+            return res.status(500);
         }
         res.send('Fileuploaded');
     });
